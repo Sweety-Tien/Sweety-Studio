8dfd82114cc6031efb00ec8c5e808dea383881f9
Sweety-Tien <miyuki911213@gmail.com>
2025-11-06 23:24:03 +0800

    change view default to small

--- sweety.js
+++ sweety.js
@@ -5,36 +5,38 @@
 
 function setupProductDensity() {
 
-  const grid = document.querySelector(".product-grid");
-  const select = document.querySelector(".product-density-select");
+    const grid = document.querySelector(".product-grid");
+    const select = document.querySelector(".product-density-select");
 
-  if(!grid || !select) return; // 有些頁面不是商品，不執行
+    if(!grid || !select) return; // 有些頁面不是商品，不執行
 
-  const isMobile = window.innerWidth < 600;
+    const isMobile = window.innerWidth < 600;
 
-  // 依裝置不同動態產生 option
-  if(isMobile){
+    // 依裝置不同動態產生 option
+    if(isMobile){
+    // 手機 → 1、2列 (預設2列)
     select.innerHTML = `
-      <option value="single">大（1列）</option>
-      <option value="large">小（2列）</option>
+        <option value="single">大（1列）</option>
+        <option value="large">小（2列）</option>
     `;
-    grid.classList.add("single");
-    select.value = "single";
-  } else {
+    grid.classList.add("large");
+    select.value = "large";
+    } else {
+    // 電腦 → 2、3、4列 (預設3列)
     select.innerHTML = `
-      <option value="large">大（2列）</option>
-      <option value="medium" selected>中（3列）</option>
-      <option value="small">小（4列）</option>
+        <option value="large">大（2列）</option>
+        <option value="medium">中（3列）</option>
+        <option value="small">小（4列）</option>
     `;
     grid.classList.add("medium");
     select.value = "medium";
-  }
+    }
 
-  // user 切換時更新 grid class
-  select.addEventListener("change", ()=>{
+    // user 切換時更新 grid class
+    select.addEventListener("change", ()=>{
     grid.classList.remove("single","large","medium","small");
     grid.classList.add(select.value);
-  });
+    });
 }
 
 // DOM Ready
